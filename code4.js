6f61b1aeaf6e0bb025a2be7a15c0633406480532
lucetaseo <101718150+lucetaseo@users.noreply.github.com>
2024-05-15 07:45:10 +0900

    4
    4

---
+++ code4.js
@@ -0,0 +1,18 @@
+function solution(num1, num2) {
+    if(num1 < num2){
+        return -1;
+    }
+        else if (num1 > num2){
+            return -1;
+        }
+        else {
+            return 1;
+        }
+}
+
+
+//function solution(num1, num2) {
+//    var answer = num1 === num2 ? 1 : -1;
+//    return answer;
+//}
+// 더 깔끔한 답!
