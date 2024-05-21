029f177afe9e2c6696d6125949d029ebf3d94aa2
lucetaseo <101718150+lucetaseo@users.noreply.github.com>
2024-05-21 09:08:28 +0900

    7
    7

---
+++ code7.js
@@ -0,0 +1,17 @@
+function solution(angle) {
+    if (angle > 0 && angle < 90) {
+        return 1;  
+    } else if (angle === 90) {
+        return 2;  
+    } else if (angle > 90 && angle < 180) {
+        return 3;  
+    } else if (angle === 180) {
+        return 4;  
+    } else {
+        return -1; 
+    }
+}
+
+console.log(solution(70));
+console.log(solution(91));  
+console.log(solution(180)); 
